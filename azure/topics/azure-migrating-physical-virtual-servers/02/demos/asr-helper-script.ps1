7e40fc461debd585b3a7685961de3a03a750a5de
Gdykeman <gerald.dykeman@gmail.com>
2019-07-12 16:30:19 -0400

    rename

---
+++ azure/topics/azure-migrating-physical-virtual-servers/02/demos/asr-helper-script.ps1
@@ -0,0 +1,17 @@
+﻿# create VM list
+ise .\vmlist.txt
+
+# Discover Hyper-V hosts and VMs
+.\ASRDeploymentPlanner.exe -Operation GetVMlist -ServerListFile ".\vmlist.txt" -Virtualization Hyper-V -User "company\tim" -OutputFile "C:\ASR\VMListFileOutput2.txt"
+ise .\VMListFileOutput.txt
+
+# Profile for 60 minutes
+.\ASRDeploymentPlanner.exe -Operation StartProfiling -Virtualization Hyper-V -Directory "C:\ASR\ProfiledData" -VMListFile "C:\ASR\VMListFileOutput.txt"  -NoOfMinutesToProfile 60 -User "company\tim"
+
+# Generate the report
+.\ASRDeploymentPlanner.exe -Operation GenerateReport -Virtualization Hyper-V -Directory "C:\ASR\ProfiledData" -VMListFile "C:\ASR\VMListFileOutput.txt"
+
+
+
+
+
